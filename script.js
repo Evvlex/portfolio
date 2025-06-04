e196743d4515ada4eda999012bc397d9f76ee16b
Evvlex <pedashdaniil@gmail.com>
2025-06-04 03:01:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('nav a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+  
+      const targetID = this.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetID);
+  
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        });
+      }
+    });
+  });
+  
